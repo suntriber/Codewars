bd885b3e720f6939a94465b2eda5d1e2eb450489
Patric <suntriber@hotmail.com>
2022-03-17 14:55:09 +0100

    Deodorant Evaporator

---
+++ C/7kyu/Deodorarnt Evaporator/cmake-build-debug/CMakeFiles/Deodorarnt_Evaporator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deodorarnt_Evaporator.dir/main.c.obj"
+  "Deodorarnt_Evaporator.exe"
+  "Deodorarnt_Evaporator.exe.manifest"
+  "Deodorarnt_Evaporator.pdb"
+  "libDeodorarnt_Evaporator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Deodorarnt_Evaporator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/7kyu/Deodorarnt Evaporator/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* Function prototype */
+int evaporator(double content, double evap_per_day, double threshold);
+
+
+int main() {
+    printf("%d\n", evaporator(10,10,10));
+    printf("%d\n", evaporator(10,10,5));
+    return 0;
+}
+
+
+int evaporator(double content, double evap_per_day, double threshold) {
+    // content = ml
+    // evap_per_day = percent
+    // percent
+    content = 1.0;
+    evap_per_day *= 0.01;
+    threshold *= 0.01;
+    int i = 0;
+    while (content > threshold){
+        content = content * (1.0-evap_per_day);
+        i++;
+        printf("content %f after %d iterations\n", content, i);
+    }
+    return i;
+}
