f65e927588da1230da668efbaefa044f7fa074e9
Patric <suntriber@hotmail.com>
2022-07-13 10:57:05 +0200

    Two oldest ages golang

---
+++ Golang/Two oldest ages.go
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/511f11d355fe575d2c000001/train/go
+*/
+
+package main
+
+import (
+	"sort"
+)
+
+func TwoOldestAges(ages []int) [2]int {
+
+	sort.Ints(ages)
+
+	return [2]int{ages[len(ages)-2], ages[len(ages)-1]}
+}
--- Golang/main.go
+++ Golang/main.go
@@ -1,8 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 
-    fmt.Println("Hello, World!")
+	TwoOldestAges([]int{1, 3, 9, 5, 7})
 }
