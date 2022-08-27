e88875796b8b792e8f9acfffbc5eb6e68119f126
Patric Sandberg <patrics@axis.com>
2022-08-27 22:16:36 +0200

    nah

--- Golang/main.go
+++ Golang/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 
-	// fmt.Println(TwoOldestAges([]int{1, 3, 9, 5, 7}))
-	// fmt.Println(sevenkuy.Calc("abcdef"))
+	fmt.Println(sevenkyu.TwoOldestAges([]int{1, 3, 9, 5, 7}))
+	fmt.Println(sevenkyu.Calc("abcdef"))
 	fmt.Println(sevenkyu.Calc("aaaaaddddr"))
 
 }
