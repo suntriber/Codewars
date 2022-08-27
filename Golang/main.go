08624a058677d587cb4e9420683ff2f9f6b295f4
Patric Sandberg <patrics@axis.com>
2022-08-27 21:57:29 +0200

    spelluing is unportant

--- Golang/main.go
+++ Golang/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"Golang/sevenkuy"
+	"codewars/sevenkyu"
 	"fmt"
 )
 
 func main() {
 
 	// fmt.Println(TwoOldestAges([]int{1, 3, 9, 5, 7}))
-	fmt.Println(sevenkuy.Calc("abcdef"))
-	fmt.Println(sevenkuy.Calc("aaaaaddddr"))
+	// fmt.Println(sevenkuy.Calc("abcdef"))
+	fmt.Println(sevenkyu.Calc("aaaaaddddr"))
+
 }
