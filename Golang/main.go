05facde88044f8b4356a62b0856806a65d274345
suntriber <suntriber@hotmail.com>
2022-08-28 09:15:39 +0200

    Hello

--- Golang/main.go
+++ Golang/main.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Println(sevenkyu.TwoOldestAges([]int{1, 3, 9, 5, 7}))
 	fmt.Println(sevenkyu.Calc("abcdef"))
 	fmt.Println(sevenkyu.Calc("aaaaaddddr"))
-
+        fmt.Println("Hello, World!")
 }
