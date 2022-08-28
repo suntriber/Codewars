2f0f29f5e9527031bebc6c76e9ff1096e6a8039a
suntriber <suntriber@hotmail.com>
2022-08-28 09:42:05 +0200

    effing globalprotect

--- Golang/main.go
+++ Golang/main.go
@@ -10,6 +10,4 @@ func main() {
 	fmt.Println(sevenkyu.TwoOldestAges([]int{1, 3, 9, 5, 7}))
 	fmt.Println(sevenkyu.Calc("abcdef"))
 	fmt.Println(sevenkyu.Calc("aaaaaddddr"))
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
 }
